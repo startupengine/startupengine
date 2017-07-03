a1bcb9ea43a607a7fe7a5667ec39cd0088785ae0
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-03 00:56:41 -0700

    Various

--- public/js/app.js
+++ public/js/app.js
@@ -41405,7 +41405,7 @@ var Component = __webpack_require__(37)(
   /* moduleIdentifier (server only) */
   null
 )
-Component.options.__file = "/Users/jhc/htdocs/slidecms/resources/assets/js/components/Example.vue"
+Component.options.__file = "/Users/jhc/htdocs/slideblogger/resources/assets/js/components/Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key.substr(0, 2) !== "__"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
@@ -41416,9 +41416,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-d8c56664", Component.options)
+    hotAPI.createRecord("data-v-9e0ad6f6", Component.options)
   } else {
-    hotAPI.reload("data-v-d8c56664", Component.options)
+    hotAPI.reload("data-v-9e0ad6f6", Component.options)
   }
   module.hot.dispose(function (data) {
     disposed = true
@@ -41579,7 +41579,7 @@ module.exports.render._withStripped = true
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-     require("vue-hot-reload-api").rerender("data-v-d8c56664", module.exports)
+     require("vue-hot-reload-api").rerender("data-v-9e0ad6f6", module.exports)
   }
 }
 
--- resources/views/sections/longform.blade.php
+++ resources/views/sections/longform.blade.php
@@ -2,6 +2,14 @@
     <!--.wrap.longform (width:72rem=720px) = Better reading experience (90-95 characters per line) -->
     <div class="wrap longform">
         <?php echo @markdown($section->getContent()); ?>
+            <?php if($section->getImage()->getFile()->getUrl() !== NULL) { ?>
+            <figure class="text-pull" >
+                <img src="<?php echo $section->getImage()->getFile()->getUrl(); ?>" style="border-radius:4px;">
+                <figcaption>
+                    <p><?php echo $section->getImage()->getTitle(); ?></p>
+                </figcaption>
+            </figure>
+            <?php } ?>
         @foreach($section->getComponents() as $component)
             <?php $type = $component->getContentType()->getName();?>  @include('components.'.strtolower($type))
         @endforeach
