b1cba261c955616a4d6371d24ca77aa75123a059
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-04 03:53:34 -0700

    Alignment

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,5 @@
-<header role="banner" style="opacity:1;background:transparent;" class="<?php if($defaults !== NULL && $defaults->getNavStyle() !== NULL) { echo strtolower($defaults->getNavStyle()); } ?>">
-    <nav role="navigation" class="navbar hiddenOnMobile">
+<header role="banner" style="opacity:1;background:transparent;padding-left:35px;padding-right:35px;" class="<?php if($defaults !== NULL && $defaults->getNavStyle() !== NULL) { echo strtolower($defaults->getNavStyle()); } ?>">
+    <nav role="navigation" class="navbar hiddenOnMobile" style="width: 100%;">
         <ul>
             <?php if($defaults !== NULL && $defaults->getLogo() !== NULL) { ?>
             <li class="">
@@ -46,8 +46,8 @@
             <? } ?>
         </ul>
     </nav>
-    <nav role="navigation" class="hiddenOnDesktop">
-        <ul class="hiddenOnDesktop">
+    <nav role="navigation" class="hiddenOnDesktop" style="width:100%;">
+        <ul class="hiddenOnDesktop" style="margin-right:11px;">
             <li class="showMobileNav menuButton">
                 <a href="#" style="font-size:150%;">
                     <svg class="fa-bars" style="margin: 15px 5px;">
