dbb6b505577e84b2da92b71623d3c682366a3bbf
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-04 00:06:59 -0700

    Style

--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -1,5 +1,5 @@
 <?php if($defaults !== NULL && $defaults->getEnableDisqus() !== NULL && $article->getComments() == TRUE) { ?>
-<section class="">
+<section class="" style="min-height:auto !important;">
     <!--.wrap = container width: 90% -->
     <div class="wrap" align="center" style="padding:0% 10%;">
         <?php echo $defaults->getDisqusCode(); ?>
