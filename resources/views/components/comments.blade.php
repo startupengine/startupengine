c02254044e361f53ca7375086bc669f0ffe97430
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-03 23:53:15 -0700

    Layout

--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -1,3 +1,8 @@
 <?php if($defaults !== NULL && $defaults->getEnableDisqus() !== NULL && $article->getComments() == TRUE) { ?>
-    <?php echo $defaults->getDisqusCode(); ?>
+<section class="<?php echo $splash; ?>">
+    <!--.wrap = container width: 90% -->
+    <div class="wrap" align="center" style="padding:0% 10%;">
+        <?php echo $defaults->getDisqusCode(); ?>
+    </div>
+</section>
 <?php } ?>
