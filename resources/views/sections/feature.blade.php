3d3f37d04610fc4f238fa700774c41b0e528daf5
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-05 19:45:20 -0700

    Tweaks

--- resources/views/sections/feature.blade.php
+++ resources/views/sections/feature.blade.php
@@ -6,16 +6,22 @@
                 <figure><img class="aligncenter" src="<?php echo $section->getImage()->getFile()->getUrl(); ?>" alt="<?php echo $section->getImage()->getTitle(); ?>"></figure>
             </div>
             <div class="column">
+                <?php if($section->getHeading() !== NULL) { ?>
                 <h1>
                     <?php echo $section->getHeading(); ?>
                 </h1>
+                <?php } ?>
+                <?php if($section->getSubHeading() !== NULL) { ?>
                 <p class="text-intro"><?php echo $section->getSubHeading(); ?></p>
+                <?php } ?>
                 <p><?php echo $section->getContent(); ?></p>
+                <?php if($section->getButtonText() !== NULL) { ?>
                 <p align="left">
                     <a href="<?php echo $section->getButtonLink(); ?>" class="button ga-track" data-ga-text="<?php echo $section->getButtonText(); ?>" title="<?php echo $section->getButtonText(); ?>">
                         <?php echo $section->getButtonText(); ?>
                     </a>
                 </p>
+                <?php } ?>
             </div>
         </div>
         <!-- end .grid-->
