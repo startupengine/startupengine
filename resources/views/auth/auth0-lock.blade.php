d6cdb3634b3e843d0333ef2d1d9ce8a2b6992aa9
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-06 15:29:47 -0700

    Auth0

--- resources/views/auth/auth0-lock.blade.php
+++ resources/views/auth/auth0-lock.blade.php
@@ -1,12 +1,13 @@
 <div id="root" style="width: 320px; margin: 40px auto; padding: 10px; border-style: solid; border-width: 1px; box-sizing: border-box;">
     embedded area
 </div>
+<?php $redirecturl = \Config::get('app.url').'/auth0/callback'; ?>
 <script src="https://cdn.auth0.com/js/lock/10.17/lock.min.js"></script>
 <script>
     var lock = new Auth0Lock('<?php echo \Config::get('laravel-auth0.client_id') ?>', '<?php echo \Config::get('laravel-auth0.domain') ?>', {
         container: 'root',
         auth: {
-            redirectUrl: '<?php echo \Config::get('app.url'); ?>/auth0/callback',
+            redirectUrl: '<?php echo $redirecturl; ?>',
             responseType: 'code',
             params: {
                 scope: 'openid email' // Learn about scopes: https://auth0.com/docs/scopes
