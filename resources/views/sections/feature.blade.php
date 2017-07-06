c2f2db3eccdb6aee9e5f06a21021bb04dfd65f04
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-05 20:24:05 -0700

    Layout

--- resources/views/sections/feature.blade.php
+++ resources/views/sections/feature.blade.php
@@ -16,7 +16,7 @@
                 <?php if($section->getSubHeading() !== NULL) { ?>
                 <p class="text-intro"><?php echo $section->getSubHeading(); ?></p>
                 <?php } ?>
-                <p align="left"><?php echo @markdown($section->getContent()); ?></p>
+                <div align="left"><?php echo @markdown($section->getContent()); ?></div>
                 <?php if($section->getButtonText() !== NULL) { ?>
                 <p>
                     <a href="<?php echo $section->getButtonLink(); ?>" class="button ga-track" data-ga-text="<?php echo $section->getButtonText(); ?>" title="<?php echo $section->getButtonText(); ?>">
