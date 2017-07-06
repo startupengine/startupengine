89a5a0e47d6dc08dd3261a1293ddc41c25ba015d
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-05 22:26:44 -0700

    Tweaks

--- resources/views/components/lightbox.blade.php
+++ resources/views/components/lightbox.blade.php
@@ -1,4 +1,4 @@
-<div class="ui modal" id="lightbox">
+<div class="ui modal" id="lightbox" style="max-width:90%;max-height:90%;">
     <i class="close icon"></i>
     <div class="content">
         <img src="http://images.contentful.com/x5o3atz1wqhm/4wm98SmiPYo4YOm8SOa2qe/635e9b39f62fd69d9098ea762335c389/danka-peter-469.jpg" id="lightboxImage">
