0122c1f039fefa99b741483f3de39bc22a5fd0ef
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-07 00:28:30 -0700

    Style

--- resources/views/components/lightbox.blade.php
+++ resources/views/components/lightbox.blade.php
@@ -1,5 +1,5 @@
 <div class="ui modal" id="lightbox" style="">
-    <i class="close icon"></i>
+    <i class="close icon" style=""></i>
     <div id="lightboxImage" style="border:10px solid #fff;width:100%;height:500px;background-size:cover;background-position:center;">
     </div>
 </div>
