24a3971162f2f829cd27506c517747f602162a88
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-08 18:45:14 -0700

    Typo

--- resources/views/auth/auth0-lock.blade.php
+++ resources/views/auth/auth0-lock.blade.php
@@ -4,7 +4,7 @@
         <div id="root" style="width: 320px; margin: 40px auto; padding: 10px; border-style: none; border-width: 1px; box-sizing: border-box;">
             embedded area
         </div>
-        <?php $redirecturl = \Config::get('app.url').'/auth0/callback'; ?>
+        <?php $redirecturl = \Config::get('app.url').'auth0/callback'; ?>
         <script src="https://cdn.auth0.com/js/lock/10.17/lock.min.js"></script>
         <script>
             var lock = new Auth0Lock('<?php echo \Config::get('laravel-auth0.client_id') ?>', '<?php echo \Config::get('laravel-auth0.domain') ?>', {
