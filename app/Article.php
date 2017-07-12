56af627ab4368165e59fc7c97c12894fd8c49cad
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-12 15:20:33 -0700

    Search config

--- app/Article.php
+++ app/Article.php
@@ -10,4 +10,28 @@ class Article extends Model
 
     use Searchable;
 
+    /**
+     * Get the index name for the model.
+     *
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'articles_index';
+    }
+
+    /**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        // Customize array...
+
+        return $array;
+    }
+
 }
