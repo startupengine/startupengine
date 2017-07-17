b51986ee8206528c10bd89b25b7a68b66cded1da
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-16 22:10:15 -0700

    Routes

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth as Auth;
 
 class AppController extends Controller
 {
     public function login()
     {
-        Auth::logout();
+        \Auth::logout();
         return \App::make('auth0')->login();
     }
 }
