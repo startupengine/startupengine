9c927a0d0da8481884ad4a384a786f11dda843a1
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-17 09:55:05 -0700

    Analytics config

--- config/analytics.php
+++ config/analytics.php
@@ -11,7 +11,7 @@ return [
      * Path to the client secret json file. Take a look at the README of this package
      * to learn how to get this file.
      */
-    'service_account_credentials_json' => (file_get_contents( env('GOOGLE_ANALYTICS_CREDENTIALS', storage_path('app/google/analytics-credentials.json') )),
+    'service_account_credentials_json' => file_get_contents(env('GOOGLE_ANALYTICS_CREDENTIALS', storage_path('app/google/analytics-credentials.json'))),
 
     /*
      * The amount of minutes the Google API responses will be cached.
