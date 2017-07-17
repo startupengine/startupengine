c65a71eaeadb6ea9323f67cef6ea814b93c95ea9
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-17 14:15:14 -0700

    Database config

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
