171856dcf63867df9c4f4098f0348b933deab736
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-07-24 22:38:46 -0700

    Cleanup

--- resources/views/admin/campaign.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app-semanticui')
-@section('content')
-    <div class="ui grid sixteen wide">
-        <div class="sixteen wide column" align="center">
-                <p class="header" style="margin-bottom:35px;padding-bottom:0px;">Your Campaigns</p>
-        </div>
-    </div>
-@endsection
