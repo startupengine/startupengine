a130b577a801ae6d42b593c32aa80da35b9761bd
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-08-09 00:57:02 -0700

    Refactoring

--- resources/views/help/index.blade.php
+++ resources/views/help/index.blade.php
@@ -1,34 +1,6 @@
 @extends('layouts.webslides')
 
 @section('title')Articles@endsection
-@section('styles')
-    <style>
-        section.bg-gradient-h .button.ghost, section.bg-gradient-v .button.ghost, section.bg-gradient-r .button.ghost{
-            border-color:#fff !important;
-            font-weight:normal !important;
-        }
-        section.bg-gradient-h .button.ghost:hover, section.bg-gradient-v .button.ghost:hover, section.bg-gradient-r .button.ghost:hover {
-            color:#222 !important;
-            background:#fff !important;
-        }
-        @media (min-width: 768px) {
-            section:first-of-type {
-                padding-top:100px !important;
-                padding-bottom:100px !important;
-            }
-        }
-        @media (max-width: 768px) {
-            section:first-of-type {
-                padding-top:75px !important;
-                padding-bottom:75px !important;
-            }
-        }
-        body {
-            background:#fff;
-        }
-
-    </style>
-@endsection
 
 @section('content')
     <article>
