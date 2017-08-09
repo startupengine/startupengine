f5778023ee214f35f503a20e7299b935ea99b233
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-08-09 00:49:14 -0700

    Debugging

--- resources/views/help/view.blade.php
+++ resources/views/help/view.blade.php
@@ -8,18 +8,16 @@
 
     <!-- FACEBOOK -->
     <meta property="og:url" content="{{ \Request::url() }}"> <!-- YOUR URL -->
-    <meta property="og:type" content="article">
+    <meta property="og:type" content="help">
     <meta property="og:title" content="<?php echo $page->getTitle(); ?>"> <!-- EDIT -->
-    <meta property="og:description" content="<?php echo $page->getDescription(); ?>"> <!-- EDIT -->
+    <meta property="og:description" content="<?php echo $page->getContent(); ?>"> <!-- EDIT -->
     <meta property="og:updated_time" content="{{ \Carbon\Carbon::now()->toFormattedDateString() }}"> <!-- EDIT -->
     <meta property="og:image" content="<?php if($page->getFeaturedImage() !== null) { echo $page->getFeaturedImage()->getFile()->getUrl(); } ?>"> <!-- EDIT -->
 
     <!-- TWITTER -->
     <meta name="twitter:card" content="<?php if($page->getFeaturedImage() !== null) { echo $page->getFeaturedImage()->getFile()->getUrl(); } ?>">
-    <meta name="twitter:site" content="@webslides"> <!-- EDIT -->
-    <meta name="twitter:creator" content="@jlantunez"> <!-- EDIT -->
     <meta name="twitter:title" content="<?php echo $page->getTitle(); ?>"> <!-- EDIT -->
-    <meta name="twitter:description" content="<?php echo $page->getDescription(); ?>"> <!-- EDIT -->
+    <meta name="twitter:description" content="<?php echo $page->getContent(); ?>"> <!-- EDIT -->
     <meta name="twitter:image" content="<?php if($page->getFeaturedImage() !== null) { echo $page->getFeaturedImage()->getFile()->getUrl(); } ?>"> <!-- EDIT -->
 @endsection
 
