1f2bfce61dc8c30204b4e67a5551cc2a6fd51df8
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-08-13 15:46:08 -0700

    Pointer events

--- resources/views/sections/splash.blade.php
+++ resources/views/sections/splash.blade.php
@@ -3,14 +3,14 @@
     <!-- Overlay/Opacity: [class*="bg-"] > .background.dark or .light -->
     <span id="particles-js" class="background dark" style="<?php if($page->getFeaturedImage() !== null) { ?>background-image:url('<?php echo $page->getFeaturedImage()->getFile()->getUrl();  ?>');<?php } ?>"></span>
     <!--.wrap = container width: 90% -->
-    <div class="wrap zoomIn" align="center" style="padding:75px 10%;min-width:300px;">
+    <div class="wrap zoomIn" align="center" style="pointer-events:none !important;padding:75px 10%;min-width:300px;">
         <h1 style="margin-bottom:15px;">
             <strong><?php echo $section->getTitle(); ?></strong>
         </h1>
         <?php if($section->getSubtitle() !== NULL && $section->getSubtitle() !== '') { ?><div class="text-subtitle"><?php echo @markdown($section->getSubtitle()) ?></div><?php } ?>
         <p>
             <?php $headerCTA = $section->getButtonText(); if($headerCTA == NULL) { $headerCTA = 'Read Article'; } ?>
-            <a href="#section-{{ $count + 1 }}" class="button radius <?php if(\Request::capture()->getRequestUri() == '/') { echo "ghost"; } ?>  ga-track" data-ga-category="{{ $analyticsCategory }}" data-ga-action="button" data-ga-label="<?php if($page->getCampaign() !== null) { echo $page->getCampaign()->getSlug(); } else { echo $headerCTA;  } ?>" data-ga-text="{{ $headerCTA }}" title="{{ $headerCTA }}">
+            <a href="#section-{{ $count + 1 }}" style="pointer-events:all !important;" class="button radius <?php if(\Request::capture()->getRequestUri() == '/') { echo "ghost"; } ?>  ga-track" data-ga-category="{{ $analyticsCategory }}" data-ga-action="button" data-ga-label="<?php if($page->getCampaign() !== null) { echo $page->getCampaign()->getSlug(); } else { echo $headerCTA;  } ?>" data-ga-text="{{ $headerCTA }}" title="{{ $headerCTA }}">
                 {{ $headerCTA }}
             </a>
         </p>
