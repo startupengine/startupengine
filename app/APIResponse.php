3268d06502a04345a263a141a97f037c63f6d8af
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-18 02:16:52 -0700

    Case

--- app/APIResponse.php
+++ app/APIResponse.php
@@ -17,7 +17,7 @@ class APIResponse extends Model
         $fields = 'id, status, title, meta_description, slug, image';
         $items = \DB::table($type)
             ->select(\DB::raw($fields))
-            ->where('status', '=', 'published')
+            ->where('status', '=', 'PUBLISHED')
             ->where('category_id', '=', $category->id)
             ->limit($limit)
             ->orderBy('created_at')
@@ -42,7 +42,7 @@ class APIResponse extends Model
         $fields = 'id, status, title, meta_description, slug, image';
         $items = \DB::table($type)
             ->select(\DB::raw($fields))
-            ->where('status', '=', 'published')
+            ->where('status', '=', 'PUBLISHED')
             ->limit($limit)
             ->orderBy('created_at')
             ->get();
@@ -66,7 +66,7 @@ class APIResponse extends Model
         $items = \DB::table($type)
             ->select(\DB::raw($fields))
             ->where('slug', '=', $slug)
-            ->where('status', '=', 'published')
+            ->where('status', '=', 'PUBLISHED')
             ->orderBy('created_at')
             ->get();
 
