18532be1921c5ea95943ccbaf8a2cb4c5266e4a8
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-17 18:00:41 -0700

    User seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PostsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
