3a5d0ab61da99ccf9ac58e3d71fabc8cbc58448c
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-10-21 23:37:38 -0700

    Github API Package

---
+++ config/github.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Laravel GitHub.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the connections below you wish to use as
+    | your default connection for all work. Of course, you may use many
+    | connections at once using the manager class.
+    |
+    */
+
+    'default' => 'main',
+
+    /*
+    |--------------------------------------------------------------------------
+    | GitHub Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the connections setup for your application. Example
+    | configuration has been included, but you may add as many connections as
+    | you would like. Note that the 4 supported authentication methods are:
+    | "application", "jwt", "password", and "token".
+    |
+    */
+
+    'connections' => [
+
+        'main' => [
+            'token'      => env('GITHUB_TOKEN'),
+            'method'     => 'token',
+            // 'backoff'    => false,
+            // 'cache'      => false,
+            // 'version'    => 'v3',
+            // 'enterprise' => false,
+        ],
+
+        'app' => [
+            'clientId'     => env('GITHUB_CLIENT_ID'),
+            'clientSecret' => env('GITHUB_CLIENT_SECRET'),
+            'method'       => 'application',
+            // 'backoff'      => false,
+            // 'cache'        => false,
+            // 'version'      => 'v3',
+            // 'enterprise'   => false,
+        ],
+
+        'jwt' => [
+            'token'        => env('GITHUB_JWT_TOKEN'),,
+            'method'       => 'jwt',
+            // 'backoff'      => false,
+            // 'cache'        => false,
+            // 'version'      => 'v3',
+            // 'enterprise'   => false,
+        ],
+
+        'other' => [
+            'username'   => env('GITHUB_USERNAME'),
+            'password'   => env('GITHUB_PASSWORD'),
+            'method'     => 'password',
+            // 'backoff'    => false,
+            // 'cache'      => false,
+            // 'version'    => 'v3',
+            // 'enterprise' => false,
+        ],
+
+    ],
+
+];
