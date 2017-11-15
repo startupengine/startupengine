266e5a05dc4028483bb59bff6db7698dbf50ffaf
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-11-15 12:44:26 -0800

    Form weirdness

--- resources/views/app/post/edit.blade.php
+++ resources/views/app/post/edit.blade.php
@@ -60,7 +60,7 @@
                                             <?php $category = \App\Category::find($post->category_id);?>
                                             <select class="custom-select" id="category_id" name="category_id" aria-describedby="potCategory" style="width:100%;">
                                                 <?php if($category!== null ) { ?>
-                                                    <option selected disabled>{{$category->name}}</option>
+                                                    <option value="{{ $category->id }}" selected disabled>{{$category->name}}</option>
                                                 <?php } else { ?>
                                                     <option selected disabled>Choose a category</option>
                                                 <?php } ?>
