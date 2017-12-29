72f4891b9f85847c4039ae3313288dae8de6d1ef
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-29 02:17:58 -0800

    UI tweaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -291,9 +291,10 @@
             border-bottom: 3px solid #fff;
             color: #fff;
             position: fixed;
-            top: 15px;
-            left: 15px;
-            width: calc(100% - 30px) !important;
+            top: 0px;
+            left: 0px;
+            border-radius:0px !important;
+            width: 100% !important;
             z-index: 3 !important;
         }
 
@@ -372,7 +373,7 @@
 
         .card tr:hover {
             color: #111 !important;
-            background:#f7f9fd !important;
+            background: #f7f9fd !important;
         }
 
         table th {
@@ -453,115 +454,127 @@
         th {
             background: #fff !important;
         }
+
         .CodeMirror, .CodeMirror-scroll {
             min-height: 30px;
+        }
+
+        #app > .container-fluid:first-of-type {
+            margin:0px !important;
+            padding:0px !important;
+        }
+
+        #app > .card:first-of-type {
+            border-radius:0px !important;
+            margin:0px !important;
+            padding:0px !important;
         }
     </style>
 
     @yield('styles')
 </head>
 <body>
-    <div id="app">
-        @yield('content')
-    </div>
+<div id="app" style="display: block; overflow-y: scroll !important; position: absolute;top: 0px; left: 0px; width: 100%; height: 100vh;">
+    @yield('content')
+</div>
 
-    <!-- UX -->
-    <script type="text/javascript">
-        function scrollToMore() {
-            if ($('#more').length != 0) {
-                $("html, body").animate({
-                    scrollTop: $('#more').offset().top - 75
-                }, 500);
-            }
+<!-- UX -->
+<script type="text/javascript">
+    function scrollToMore() {
+        if ($('#more').length != 0) {
+            $("html, body").animate({
+                scrollTop: $('#more').offset().top - 75
+            }, 500);
         }
+    }
 
-        $(document).ready(function () {
+    $(document).ready(function () {
 
-        });
-    </script>
+    });
+</script>
 
-    <!-- Mixpanel -->
-    <script type="text/javascript">
-        (function (e, a) {
-            if (!a.__SV) {
-                var b = window;
-                try {
-                    var c, l, i, j = b.location, g = j.hash;
-                    c = function (a, b) {
-                        return (l = a.match(RegExp(b + "=([^&]*)"))) ? l[1] : null
-                    };
-                    g && c(g, "state") && (i = JSON.parse(decodeURIComponent(c(g, "state"))), "mpeditor" === i.action && (b.sessionStorage.setItem("_mpcehash", g), history.replaceState(i.desiredHash || "", e.title, j.pathname + j.search)))
-                } catch (m) {
-                }
-                var k, h;
-                window.mixpanel = a;
-                a._i = [];
-                a.init = function (b, c, f) {
-                    function e(b, a) {
-                        var c = a.split(".");
-                        2 == c.length && (b = b[c[0]], a = c[1]);
-                        b[a] = function () {
-                            b.push([a].concat(Array.prototype.slice.call(arguments,
-                                0)))
-                        }
-                    }
-
-                    var d = a;
-                    "undefined" !== typeof f ? d = a[f] = [] : f = "mixpanel";
-                    d.people = d.people || [];
-                    d.toString = function (b) {
-                        var a = "mixpanel";
-                        "mixpanel" !== f && (a += "." + f);
-                        b || (a += " (stub)");
-                        return a
-                    };
-                    d.people.toString = function () {
-                        return d.toString(1) + ".people (stub)"
-                    };
-                    k = "disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
-                    for (h = 0; h < k.length; h++)e(d, k[h]);
-                    a._i.push([b, c, f])
+<!-- Mixpanel -->
+<script type="text/javascript">
+    (function (e, a) {
+        if (!a.__SV) {
+            var b = window;
+            try {
+                var c, l, i, j = b.location, g = j.hash;
+                c = function (a, b) {
+                    return (l = a.match(RegExp(b + "=([^&]*)"))) ? l[1] : null
                 };
-                a.__SV = 1.2;
-                b = e.createElement("script");
-                b.type = "text/javascript";
-                b.async = !0;
-                b.src = "undefined" !== typeof MIXPANEL_CUSTOM_LIB_URL ? MIXPANEL_CUSTOM_LIB_URL : "file:" === e.location.protocol && "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//) ? "https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js" : "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";
-                c = e.getElementsByTagName("script")[0];
-                c.parentNode.insertBefore(b, c)
+                g && c(g, "state") && (i = JSON.parse(decodeURIComponent(c(g, "state"))), "mpeditor" === i.action && (b.sessionStorage.setItem("_mpcehash", g), history.replaceState(i.desiredHash || "", e.title, j.pathname + j.search)))
+            } catch (m) {
             }
-        })(document, window.mixpanel || []);
-        mixpanel.init("c3af8ab4b008e22adf4772978aca66ec");
+            var k, h;
+            window.mixpanel = a;
+            a._i = [];
+            a.init = function (b, c, f) {
+                function e(b, a) {
+                    var c = a.split(".");
+                    2 == c.length && (b = b[c[0]], a = c[1]);
+                    b[a] = function () {
+                        b.push([a].concat(Array.prototype.slice.call(arguments,
+                            0)))
+                    }
+                }
 
-        function clickEvent(id, name, title, url, type, src, text) {
-            mixpanel.track("click", {
-                "id": id,
-                "name": name,
-                "title": title,
-                "url": url,
-                "type": type,
-                "src": src,
-                "text": text
-            });
+                var d = a;
+                "undefined" !== typeof f ? d = a[f] = [] : f = "mixpanel";
+                d.people = d.people || [];
+                d.toString = function (b) {
+                    var a = "mixpanel";
+                    "mixpanel" !== f && (a += "." + f);
+                    b || (a += " (stub)");
+                    return a
+                };
+                d.people.toString = function () {
+                    return d.toString(1) + ".people (stub)"
+                };
+                k = "disable time_event track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config reset people.set people.set_once people.increment people.append people.union people.track_charge people.clear_charges people.delete_user".split(" ");
+                for (h = 0; h < k.length; h++)e(d, k[h]);
+                a._i.push([b, c, f])
+            };
+            a.__SV = 1.2;
+            b = e.createElement("script");
+            b.type = "text/javascript";
+            b.async = !0;
+            b.src = "undefined" !== typeof MIXPANEL_CUSTOM_LIB_URL ? MIXPANEL_CUSTOM_LIB_URL : "file:" === e.location.protocol && "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js".match(/^\/\//) ? "https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js" : "//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";
+            c = e.getElementsByTagName("script")[0];
+            c.parentNode.insertBefore(b, c)
         }
+    })(document, window.mixpanel || []);
+    mixpanel.init("c3af8ab4b008e22adf4772978aca66ec");
 
-        $(document).ready(function () {
-            $("a, button, .btn, input, img").click(function () {
-                var id = $(this).attr('id');
-                var name = $(this).attr('name');
-                var title = $(this).attr('title');
-                var url = $(this).attr('href');
-                var type = $(this).attr('type');
-                var src = $(this).attr('src');
-                var text = $(this).html();
-                clickEvent(id, name, title, url, type, src, text);
-            });
+    function clickEvent(id, name, title, url, type, src, text) {
+        mixpanel.track("click", {
+            "id": id,
+            "name": name,
+            "title": title,
+            "url": url,
+            "type": type,
+            "src": src,
+            "text": text
         });
+    }
 
-    </script>
+    $(document).ready(function () {
+        $("a, button, .btn, input, img").click(function () {
+            var id = $(this).attr('id');
+            var name = $(this).attr('name');
+            var title = $(this).attr('title');
+            var url = $(this).attr('href');
+            var type = $(this).attr('type');
+            var src = $(this).attr('src');
+            var text = $(this).html();
+            clickEvent(id, name, title, url, type, src, text);
+        });
+    });
 
-    <!--  App -->
-    <script src="/js/app.js"></script>
+</script>
+
+<!--  App -->
+<script src="/js/app.js"></script>
 
 </body>
 </html>
