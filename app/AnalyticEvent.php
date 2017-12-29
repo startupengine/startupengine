c59d15b5a89ab9f3f767b3469a40abd90e67e5d4
huntercarter <jhc@Jamess-MacBook-Pro.local>
2017-12-28 22:18:57 -0800

    Hidden attributes

--- app/AnalyticEvent.php
+++ app/AnalyticEvent.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class AnalyticEvent extends Model
 {
-    //
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id', "segments", "decoded_path", 'cookies', "encodings", "base_path", "user_id", "client_ips", "languages", "script_name", "fingerprint", "headers", "input", "content", "attributes", "user_email", "user_name", "server", "instance", "json", "scheme_and_host", "query_string", "full_url"
+    ];
 }
