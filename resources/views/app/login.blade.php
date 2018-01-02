f434e293adb6266672ca9997a73dcbd8b3a45863
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-02 12:52:48 -0800

    Styling login errors

--- resources/views/app/login.blade.php
+++ resources/views/app/login.blade.php
@@ -11,17 +11,56 @@
 @section('styles')
     <style>
         .card-header {
-            width:100% !important;
-            min-width:100% !important;
-            position:relative !important;
-            left:0px !important;
-            top:0px !important;
+            width: 100% !important;
+            min-width: 100% !important;
+            position: relative !important;
+            left: 0px !important;
+            top: 0px !important;
         }
+
         .card {
-            background:#fff !important;
+            background: #fff !important;
         }
-        #gradient{
+
+        #gradient {
             display: none;
+        }
+
+        .card-signup {
+            /*background: orangered !important;*/
+            background: linear-gradient(45deg, #9360ff, #4286ff) !important;
+        }
+
+        .card-signup .card-body i, .card-signup h4 {
+            color: #fff !important;
+        }
+
+        #signin {
+            color: #444 !important;
+        }
+
+        .bg-gradient-light {
+            background: linear-gradient(-45deg, rgba(255, 235, 225, 0.71), #e7ecff);
+        }
+
+        .checkbox label::before, .checkbox label::after {
+            background: #fff;
+        }
+
+        .input-group-addon {
+            padding-right: 13px !important;
+            background: rgba(0, 0, 0, 0.3) !important;
+        }
+
+        input {
+            -webkit-text-fill-color: #333 !important;
+            -webkit-box-shadow: 0 0 0 30px white inset !important;
+        }
+
+        .help-block {
+            color:#fff !important;
+            text-align: center;
+            margin-bottom:15px;
         }
     </style>
 
@@ -29,62 +68,80 @@
 
 
 @section('content')
-<body class="index-page sidebar-collapse bg-gradient" style="height:100vh;">
+    <body class="index-page sidebar-collapse bg-gradient-light" style="height:100vh;">
     <div class="container" style="margin-top:15px;">
-        <div class="col-md-12" align="center">
-            <div class="card" style="max-width:500px;width:100%;margin-top:10% !important;">
-                <div class="card-header">Login</div>
-                <div class="card-body">
-                    <form class="form-horizontal " method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-                            <div class="col-md-12">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+        <div class="row" style="margin-top:10%;">
+            <div class="card card-signup bg-gradient">
+                <form class="form-horizontal " method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+                    <div class="header text-center">
+                        <h4 class="title title-up">Sign In</h4>
+                        <div class="social-line">
+
+                            <a href="#pablo" class="btn btn-neutral btn-twitter btn-icon btn-round">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                            <a href="#pablo" class="btn btn-neutral btn-facebook btn-icon btn-lg  btn-round">
+                                <i class="fa fa-facebook-square"></i>
+                            </a>
+                            <a href="#pablo" class="btn btn-neutral btn-google btn-icon btn-round">
+                                <i class="fa fa-google-plus"></i>
+                            </a>
+
+
                         </div>
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-                            <div class="col-md-12">
-                                <input id="password" type="password" class="form-control" name="password" required>
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                    </div>
+                    <div class="card-body">
+                        <div class="form-group form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            @if ($errors->has('email'))
+                                <div class="help-block" style="width:100% !important;">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </div>
+                            @endif
+                            @if ($errors->has('password'))
+                                <div class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </div>
+                            @endif
+                            <div class="input-group form-group-no-border">
+                                <span class="input-group-addon">
+                                    <i class="now-ui-icons ui-1_email-85"></i>
+                                </span>
+                                <input type="text" class="form-control" placeholder="Email..." name="email"
+                                       value="{{ old('email') }}" required autofocus>
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-12">
-                                <div class="form-check" style="margin-top:15px;">
-                                    <div class="checkbox">
-                                        <input id="remember" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        <label for="remember">
-                                            Remember Me
-                                        </label>
+                            <div class="input-group form-group-no-border">
+                                <span class="input-group-addon">
+                                    <i class="now-ui-icons ui-1_lock-circle-open"></i>
+                                </span>
+                                <input type="password" placeholder="Password..." class="form-control" required
+                                       name="password">
+                            </div>
+                            <?php /*
+                            <div class="form-group">
+                                <div class="col-md-12">
+                                    <div class="form-check" style="margin-top:15px;">
+                                        <div class="checkbox" align="center">
+                                            <input id="remember" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} >
+                                            <label for="remember" style="color:#fff;">
+                                                Remember Me
+                                            </label>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            */?>
                         </div>
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div class="footer text-center">
+                        <button class="btn btn-neutral btn-round btn-lg" id="signin">Sign In</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</body>
+    <div align="center" style="margin-top:15px;">
+        <a href="/" class="btn btn-link">Back to {{setting('site.name')}}.</a>
+    </div>
+    </body>
 @endsection
