14d0177b598632b65abb0f1681717a50eeda29ae
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-02 03:20:18 -0800

    Removing auth logic from redirect

--- app/Http/Controllers/DesignController.php
+++ app/Http/Controllers/DesignController.php
@@ -7,9 +7,6 @@ use Illuminate\Http\Request;
 class DesignController extends Controller
 {
     public function index() {
-        if (\Auth::user()->role()->name !== 'admin') {
-            abort(404);
-        }
         return redirect('/app/settings?group=Theme');
     }
 
