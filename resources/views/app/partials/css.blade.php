012150b254bac9c89ecc5bbb8277e051d376c4c1
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-09 23:07:01 -0800

    Input hover/focus state styling

--- resources/views/app/partials/css.blade.php
+++ resources/views/app/partials/css.blade.php
@@ -564,7 +564,7 @@
         font-size:13px !important;
     }
     .CodeMirror {
-        border-radius: 5px !important;
+        border-radius:5px 5px;
         font-size:13px !important;
         min-height:40px !important;
         padding:10px !important;
@@ -577,6 +577,27 @@
         resize: vertical !important;
         padding:10px !important;
 
+    }
+
+    textarea:focus, input:focus, textarea:hover, input:hover, .ace_editor:hover {
+        border:1px solid rgba(0, 0, 0, 0.35) !important;
+    }
+
+    .editor-toolbar {
+        border-radius:5px 5px 0px 0px !important;
+        border:1px solid #ddd;
+        margin-bottom:-3px;
+        z-index:0999993000 !important;
+        opacity:1 !important;
+        background: #f4f8fd !important;
+        box-shadow:none !important;
+    }
+
+    .ace_editor {
+        border-radius:5px !important;
+        z-index:0;
+        border:1px solid #eee;
+        width:100%;
     }
 
     input {
