2ec1e577eaf4315dc14e2180444939a478a8a9c5
huntercarter <jhc@Jamess-MacBook-Pro.local>
2018-01-20 02:44:57 -0800

    Sorting by date

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\AnalyticEvent;
 use Illuminate\Http\Request;
 use App\Role;
 
@@ -12,9 +13,23 @@ class AnalyticsController extends Controller
         if ($request->input('view') !== null) {
             $view = $request->input;
         } else {
-            $view = 'mixpanel';
+            $view = 'default';
         }
-        return view('app.analytics.index')->with('view', $view);
+        $analytics = AnalyticEvent::where('event_type', '!=', null)->orderBy('created_at')->get();
+
+        $analytics = $analytics->groupBy('event_type');
+
+        $analytics->transform(function ($item, $key) {
+            $item = $item->groupBy(function($date) {
+                return \Carbon\Carbon::parse($date->created_at)->format('m-d-Y'); });
+            $item->transform(function ($date, $key) {
+                $date->count = count($date);
+                return $date;
+            });
+            return $item;
+        });
+        //dd($analytics);
+        return view('app.analytics.index')->with('view', $view)->with('analytics', $analytics);
     }
 
     public function mixpanel(Request $request)
--- resources/views/app/analytics/index.blade.php
+++ resources/views/app/analytics/index.blade.php
@@ -25,7 +25,30 @@
             width: 100%;
             height: 100%;
         }
+
+        @media(min-width:991px)  {
+            .hiddenOnMobile {
+                display:block;
+            }
+            .hiddenOnDesktop{
+                display:none;
+            }
+        }
+        @media(max-width:991px)  {
+            .hiddenOnMobile {
+                display:none;
+            }
+            .hiddenOnDesktop{
+                display:block;
+            }
+        }
     </style>
+
+    @if($view == "default")
+        <script src="//cdn.jsdelivr.net/d3js/3.5.17/d3.min.js" charset="utf-8"></script>
+        <script src="//cdn.jsdelivr.net/npm/taucharts@1/build/production/tauCharts.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/taucharts@1/build/production/tauCharts.min.css">
+    @endif
 @endsection
 
 @section('content')
@@ -36,10 +59,41 @@
                 <h5 style="margin-bottom:25px;">Analytics</h5>
             </div>
             <div class="col-md-12" style="margin-bottom:40px;">
-                <div class="fluidMedia">
-                    <iframe src="/app/analytics/{{$view}}" frameborder="0" style="min-height:100%;">
-                    </iframe>
-                </div>
+                @if($view == "mixpanel")
+                    <div class="fluidMedia">
+                        <iframe src="/app/analytics/{{$view}}" frameborder="0" style="min-height:100%;">
+                        </iframe>
+                    </div>
+                @endif
+                @if($view == "default")
+                    <div class="fluidMedia">
+                        <div id="chart" class="" style="height:500px;min-height:calc(100vh - 250px);"></div>
+                        <script>
+                            var datasource = [
+                                <?php
+                                $count = 1;
+                                foreach($analytics as $eventType => $collection) {
+                                    foreach($collection as $date => $eventCount) {
+                                        echo "{type:'$eventType', date: '$date', count:'".count($eventCount)."'},";
+                                    }
+                                } ?>
+                                ];
+                            var chart = new tauCharts.Chart({
+                                data: datasource,
+                                type: 'scatterplot',
+                                x: 'date',
+                                y: 'count',
+                                size: 'count',
+                                color: 'type',
+                                plugins: [
+                                    tauCharts.api.plugins.get('tooltip')(),
+                                    tauCharts.api.plugins.get('quick-filter')()
+                                ]
+                            });
+                            chart.renderTo('#chart');
+                        </script>
+                    </div>
+                @endif
             </div>
         </div>
     </main>
