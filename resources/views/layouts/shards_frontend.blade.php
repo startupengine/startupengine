f692d8e9ea654efb16c3e6cf3ca8a0a9a8ac7a04
James Hunter Carter <james.carter@mythic.ai>
2018-12-18 17:13:40 -0800

    Cleanup

--- resources/views/layouts/shards_frontend.blade.php
+++ resources/views/layouts/shards_frontend.blade.php
@@ -313,7 +313,7 @@
                         <span v-else>Action completed successfully.</span>
                     </p>
                     <p v-else-if="instance != null && instance.transformationResult != null && instance.transformationResult.data.meta.status == 'error'">Something went wrong.</p>
-                    <p class="card-text" v-else-if="instance != null && instance.transformationResult == null && options.transformation.hasOwnProperty('confirmation_message') == true && options.transformation.confirmation_message != null" id="actionMessage">@{{ options.transformation.confirmation_message }}</p>
+                    <p class="card-text" v-else-if="instance != null && instance.transformationResult == null && options.transformation.hasOwnProperty('confirmation_message')" id="actionMessage">@{{ options.transformation.confirmation_message }}</p>
                 </div>
             </div>
             <div class="modal-footer px-3" v-if="instance !== null">
