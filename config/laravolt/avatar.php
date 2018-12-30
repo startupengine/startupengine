2f8c2920be1dc3349ad6ec326af629b1ecaeb575
James Hunter Carter <james.carter@mythic.ai>
2018-12-29 17:50:03 -0800

    Avatar resolution

--- config/laravolt/avatar.php
+++ config/laravolt/avatar.php
@@ -28,16 +28,16 @@ return [
     'shape' => 'circle',
 
     // Image width, in pixel
-    'width'    => 600,
+    'width'    => 1000,
 
     // Image height, in pixel
-    'height'   => 600,
+    'height'   => 1000,
 
     // Number of characters used as initials. If name consists of single word, the first N character will be used
     'chars'    => 2,
 
     // font size
-    'fontSize' => 220,
+    'fontSize' => 500,
 
     // convert initial letter in uppercase
     'uppercase' => false,
