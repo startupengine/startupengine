ac97a52c28f98b294e02777584d78524513a3988
James Hunter Carter <james.carter@mythic.ai>
2019-01-28 22:18:02 -0800

    Tagline

--- resources/views/docs/index_md.blade.php
+++ resources/views/docs/index_md.blade.php
@@ -175,7 +175,9 @@
         @endif
         </div>
 
-            <div class="col-md-12 text-right w-100 pr-0 pb-3">Built with <span class="mx-2">&nbsp;💖&nbsp;</span>Copyright {{ date('Y') }}</div>
+        <div class="col-md-12 text-right w-100 pr-0 pb-3">
+            Built with <span class="ml-2">&nbsp;💖&nbsp;<span class="fa fa-fw fa-sm fa-plus dimmed mx-2"></span>&nbsp;⚡</span>
+        </div>
 
     </div>
 
