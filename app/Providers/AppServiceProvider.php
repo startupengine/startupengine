5738bbeb22b22dda0d065d12a0614647cfef0329
James Hunter Carter <james.carter@mythic.ai>
2019-05-14 20:52:24 -0700

    Debugging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'production') {
+        if (\Config::get('app.env') === 'production') {
             URL::forceSchema('https');
         }
     }
