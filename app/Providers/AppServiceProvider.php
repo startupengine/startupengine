b00dc369c1b357f3814c648e43c257422f30c3ee
James Hunter Carter <james.carter@mythic.ai>
2019-05-14 20:50:30 -0700

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') === 'production') {
+        if (config('app.env') === 'production') {
             URL::forceSchema('https');
         }
     }
