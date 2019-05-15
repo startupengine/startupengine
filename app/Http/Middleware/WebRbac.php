b6fda530159cdca0dddc825d2d4a12137023c684
James Hunter Carter <james.carter@mythic.ai>
2019-05-14 21:03:49 -0700

    Debugging

--- app/Http/Middleware/WebRbac.php
+++ app/Http/Middleware/WebRbac.php
@@ -16,79 +16,75 @@ class WebRbac
      */
     public function handle($request, Closure $next)
     {
-        if (!$request->secure() && \App::environment() === 'production') {
-            return redirect()->secure($request->getRequestUri());
+        if (request()->route()->action['as'] == 'contentById') {
+            $hash = request()->route('hash');
+            $hashids = new Hashids();
+            $id = $hashids->decode($hash);
+            if (!isset($id[0])) {
+                abort(500);
+            }
+            $model = \App\Post::find($id);
+        }
+        if (request()->route()->action['as'] == 'view page') {
+            $id = request()->route('slug');
+            $model = \App\Page::where('slug', '=', $id)->first();
+        }
+
+        if (!isset($model) && !isset($id)) {
+            if (request()->route()->action['as'] != 'homepage') {
+                abort(404);
+            }
+        } elseif (
+            isset($model) &&
+            (request()->route()->action['as'] == 'view page' or
+                request()->route()->action['as'] == 'homepage') &&
+            defaultPageExists($id)
+        ) {
         } else {
-            if (request()->route()->action['as'] == 'contentById') {
-                $hash = request()->route('hash');
-                $hashids = new Hashids();
-                $id = $hashids->decode($hash);
-                if (!isset($id[0])) {
-                    abort(500);
-                }
-                $model = \App\Post::find($id);
-            }
-            if (request()->route()->action['as'] == 'view page') {
-                $id = request()->route('slug');
-                $model = \App\Page::where('slug', '=', $id)->first();
-            }
-
-            if (!isset($model) && !isset($id)) {
-                if (request()->route()->action['as'] != 'homepage') {
-                    abort(404);
-                }
-            } elseif (
-                isset($model) &&
-                (request()->route()->action['as'] == 'view page' or
-                    request()->route()->action['as'] == 'homepage') &&
-                defaultPageExists($id)
+            if (
+                method_exists($model, 'schema') &&
+                $model->schema() != null &&
+                isset($model->schema()->permissions) &&
+                isset($model->schema()->permissions->read)
             ) {
-            } else {
-                if (
-                    method_exists($model, 'schema') &&
-                    $model->schema() != null &&
-                    isset($model->schema()->permissions) &&
-                    isset($model->schema()->permissions->read)
-                ) {
-                    $rules = json_decode(
-                        json_encode($model->schema()->permissions->read),
-                        true
-                    );
+                $rules = json_decode(
+                    json_encode($model->schema()->permissions->read),
+                    true
+                );
 
-                    if (
-                        isset($rules['requires_auth']) &&
-                        $rules['requires_auth'] == true &&
-                        \Auth::user() == null
-                    ) {
-                        abort(401);
-                    }
-                    if (
-                        isset($rules['requires_permission']) &&
-                        \Auth::user() == null
-                    ) {
-                        abort(401);
-                    }
-                    if (
-                        isset($rules['requires_permission']) &&
-                        \Auth::user() != null
-                    ) {
-                        foreach ($rules['requires_permission'] as $permission) {
-                            try {
-                                if (
-                                    \Auth::user()->hasPermissionTo(
-                                        $rules['requires_permission'] !== true
-                                    )
-                                ) {
-                                    abort(403);
-                                }
-                            } catch (\Exception $exception) {
+                if (
+                    isset($rules['requires_auth']) &&
+                    $rules['requires_auth'] == true &&
+                    \Auth::user() == null
+                ) {
+                    abort(401);
+                }
+                if (
+                    isset($rules['requires_permission']) &&
+                    \Auth::user() == null
+                ) {
+                    abort(401);
+                }
+                if (
+                    isset($rules['requires_permission']) &&
+                    \Auth::user() != null
+                ) {
+                    foreach ($rules['requires_permission'] as $permission) {
+                        try {
+                            if (
+                                \Auth::user()->hasPermissionTo(
+                                    $rules['requires_permission'] !== true
+                                )
+                            ) {
                                 abort(403);
                             }
+                        } catch (\Exception $exception) {
+                            abort(403);
                         }
                     }
                 }
             }
-            return $next($request);
         }
+        return $next($request);
     }
 }
