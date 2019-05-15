71e99dbc046a111e9430436865a77e60f4a7239b
James Hunter Carter <james.carter@mythic.ai>
2019-05-14 20:55:00 -0700

    Debugging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
