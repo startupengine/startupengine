14566510e686b591fa5561523dbc612785fbda51
James Hunter Carter <james.carter@mythic.ai>
2019-05-15 16:09:09 -0700

    Conditional layout

--- resources/views/pages/defaults/home/home.blade.php
+++ resources/views/pages/defaults/home/home.blade.php
@@ -135,51 +135,55 @@
 @section('content')
 
 
-    @if(count(\App\Feature::all()) > 0)
-        <div class="container d-block">
-        @foreach(\App\Feature::where('status', 'PUBLISHED')->limit(3)->get() as $feature)
-            <div class="row inline-flex px-3 mb-0 pb-0" style="margin-top:-65px;">
-                <div class="col-md-12 px-4 pb-0 mb-0">
-                    <div class="mb-5 raised">
-                        <div class="card-body text-center bg-white text-dark br-5 p-5">
-                            <div class="row">
-                                <div class="col-md-6 m-auto">
-                                    <h5 class="text-left"><span class="underlined pb-4 d-block text-capitalize bg-white">{{ $feature->name }}</span></h5>
-                                    <img src="{!! $feature->getJsonContent('[sections][heading][fields][thumbnail]') !!}" class="hiddenOnDesktop mt-3" style="max-width:100%;"/>
-                                    <p class="card-text text-left py-4" style="font-size:150%;text-transform:unset !important;">{!! $feature->getJsonContent('[sections][heading][fields][description]') !!}</p>
-                                    <div align="left" class="mb-4"><a href="/features/{{ $feature->slug }}" class="btn btn-primary btn-pill">@if($feature->getJsonContent('[sections][heading][fields][button]') != null){!! $feature->getJsonContent('[sections][heading][fields][button]') !!}@else Read More <i class="ml-2 fa fa-fw fa-arrow-right text-warning "></i>@endif</a></div>
-                                </div>
-                                <div class="col-md-6 d-block hiddenOnMobile m-auto">
-                                    <img src="{!! $feature->getJsonContent('[sections][heading][fields][thumbnail]') !!}" style="max-width:100%;"/>
+    @if($page->getJsonContent('[sections][content][fields][show_features]') != 'no')
+        @if(count(\App\Feature::all()) > 0)
+            <div class="container d-block">
+                @foreach(\App\Feature::where('status', 'PUBLISHED')->limit(3)->get() as $feature)
+                    <div class="row inline-flex px-3 mb-0 pb-0" style="margin-top:-65px;">
+                        <div class="col-md-12 px-4 pb-0 mb-0">
+                            <div class="mb-5 raised">
+                                <div class="card-body text-center bg-white text-dark br-5 p-5">
+                                    <div class="row">
+                                        <div class="col-md-6 m-auto">
+                                            <h5 class="text-left"><span class="underlined pb-4 d-block text-capitalize bg-white">{{ $feature->name }}</span></h5>
+                                            <img src="{!! $feature->getJsonContent('[sections][heading][fields][thumbnail]') !!}" class="hiddenOnDesktop mt-3" style="max-width:100%;"/>
+                                            <p class="card-text text-left py-4" style="font-size:150%;text-transform:unset !important;">{!! $feature->getJsonContent('[sections][heading][fields][description]') !!}</p>
+                                            <div align="left" class="mb-4"><a href="/features/{{ $feature->slug }}" class="btn btn-primary btn-pill">@if($feature->getJsonContent('[sections][heading][fields][button]') != null){!! $feature->getJsonContent('[sections][heading][fields][button]') !!}@else Read More <i class="ml-2 fa fa-fw fa-arrow-right text-warning "></i>@endif</a></div>
+                                        </div>
+                                        <div class="col-md-6 d-block hiddenOnMobile m-auto">
+                                            <img src="{!! $feature->getJsonContent('[sections][heading][fields][thumbnail]') !!}" style="max-width:100%;"/>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        @endforeach
-        @if(count(\App\Feature::where('status', 'PUBLISHED')->limit(4)->get()) > 3)
-                <div class="row inline-flex px-3 mb-0 pb-0">
-                    <div class="col-md-12 px-4 pb-5 mb-0" align="center">
-                        <a href="/features" class="btn btn-secondary raised">View All Features <i class="fa fa-fw fa-arrow-right text-success ml-2"></i></a>
+                @endforeach
+                @if(count(\App\Feature::where('status', 'PUBLISHED')->limit(4)->get()) > 3)
+                    <div class="row inline-flex px-3 mb-0 pb-0">
+                        <div class="col-md-12 px-4 pb-5 mb-0" align="center">
+                            <a href="/features" class="btn btn-secondary raised">View All Features <i class="fa fa-fw fa-arrow-right text-success ml-2"></i></a>
+                        </div>
                     </div>
-                </div>
+                @endif
+            </div>
         @endif
-        @if(count(\App\Post::where('status', '=', 'PUBLISHED')->get()) > 0)
-            <div class="row px-0 pt-0" id="recentContent" v-if="info != null">
+    @endif
+    @if(count(\App\Post::where('status', '=', 'PUBLISHED')->get()) > 0  && $page->getJsonContent('[sections][content][fields][show_content]') != 'no')
+        <div class="container d-block">
+            <div class="row px-0 pt-5" id="recentContent" v-if="info != null">
                 <h5 class="text-center w-100 mb-4 mx-3 mt-0 text-dark-blue" style="">Recent Content</h5>
                 <div class="row px-3" id="contentApp" style="display:contents !important;" align="center">
                     {!! renderResourceTableHtmlDynamically(['CARD_CLASS' => 'card mb-4 mx-3', 'CARD_HEADER_FIELD' => 'title', 'CARD_BODY_FIELD' => 'excerpt', 'CARD_CONTAINER_CLASS' => 'col-md-4 mb-4', 'WRAPPER_CLASS' => "w-100", 'SHOW_TIMESTAMP' => false,  'SHOW_TAGS' => false,'SHOW_PAGINATION' => false, 'CARD_ROW_CLASS'=> 'px-4 justify-content-center', 'PATH' => '/content', 'PRIMARY_KEY' => 'item.hashid', 'WRAPPER_STYLE' => 'margin-top:20px;', 'CONTAINER_STYLE'=> 'width:calc(100%);opacity:0;']) !!}
                 </div>
             </div>
-        @endif
-        @if(count(\App\Post::where('status', 'PUBLISHED')->limit(4)->get()) > 3)
-            <div class="row inline-flex px-3 mb-0 pb-0">
-                <div class="col-md-12 px-4 pb-5 mb-0" align="center">
-                    <a href="/content" class="btn btn-secondary raised">Explore Content <i class="fa fa-fw fa-arrow-right text-success ml-2"></i></a>
+            @if(count(\App\Post::where('status', 'PUBLISHED')->limit(4)->get()) > 3)
+                <div class="row inline-flex px-3 mb-0 pb-0">
+                    <div class="col-md-12 px-4 pb-5 mb-0" align="center">
+                        <a href="/content" class="btn btn-secondary raised">Explore Content <i class="fa fa-fw fa-arrow-right text-success ml-2"></i></a>
+                    </div>
                 </div>
-            </div>
-        @endif
+            @endif
         </div>
     @endif
 
@@ -192,7 +196,6 @@
         if($tags != null){
             $tags = explode(',', $tags);
             $tags = json_encode($tags);
-
             $contentOptions['WITH_ANY_TAGS'] = $tags;
         }
     ?>
