cd95b8e00d08901cd2820de2789e17f0d8f4d270
James Hunter Carter <james.carter@mythic.ai>
2019-05-21 00:42:22 -0700

    debugging

--- resources/views/admin/components/resource_table_js.blade.php
+++ resources/views/admin/components/resource_table_js.blade.php
@@ -55,7 +55,7 @@
                 this.updateData();
             },
             undelete(item) {
-                url = '{{ $options['url'] }}/' + item.id + '/edit?&undelete=true&save=true&json={}';
+                url = '{{ appUrl() }}/' + item.id + '/edit?&undelete=true&save=true&json={}';
                 axios
                     .get(url)
                     .then(response => (item.deleted_at = null)
@@ -105,7 +105,7 @@
 
 
                 this.updateTags(this.withAnyTags, this.withAllTags, this.withoutTags);
-                var string = '{{ $options['url'] }}?' + this.filterString + '&perPage=' + this.perPage + '&limit=' + this.limit + '{!! $options['GLOBAL_FILTER'] !!}' + this.sortString + this.withAnyTagsString + this.withAllTagsString + this.withoutTagsString + '&page[number]=' + pageNumber + this.searchString;
+                var string = '{{ appUrl() }}?' + this.filterString + '&perPage=' + this.perPage + '&limit=' + this.limit + '{!! $options['GLOBAL_FILTER'] !!}' + this.sortString + this.withAnyTagsString + this.withAllTagsString + this.withoutTagsString + '&page[number]=' + pageNumber + this.searchString;
 
 
 
@@ -189,7 +189,7 @@
                         var actionString = '&action=' + action;
                     }
                     this.transformationStatus = 'loading';
-                    url = '{{ $options['url'] }}/' + id + '/transformation?transformation=' + transformation.slug + actionString @if( isset($options['FORCE_URL_ARGUMENTS']) ) + '&{{ $options['FORCE_URL_ARGUMENTS'] }}'@endif;
+                    url = '{{ appUrl() }}/' + id + '/transformation?transformation=' + transformation.slug + actionString @if( isset($options['FORCE_URL_ARGUMENTS']) ) + '&{{ $options['FORCE_URL_ARGUMENTS'] }}'@endif;
                     console.log(url);
                     axios
                         .post(url)
@@ -269,7 +269,7 @@
                 return this.newContentType;
             },
             save() {
-                url = '{{ $options['url'] }}?title=' + this.itemName + '&type=' + this.newContentType;
+                url = '{{ appUrl() }}?title=' + this.itemName + '&type=' + this.newContentType;
                 axios
                     .get(url)
                     .then(response => (window.location.href = '/admin/content/view/' + response.data.data.id)
